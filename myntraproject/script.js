141c1fae8a86899b4a266b98acdde77a4d247f0e
Srinivas0129 <srinivas.marriyavula02@gmail.com>
2021-10-27 10:31:07 +0530

    my first project

--- myntraproject/script.js
+++ myntraproject/script.js
@@ -4,7 +4,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "admin" && password == "admin"){
 alert ("Login successfully");
-window.location = "index2.html";
+window.location = "logout.html";
 return false;
 }
 else{
